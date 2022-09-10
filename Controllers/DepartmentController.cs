fdf712588406604bf51289ac4c24d46fdebaf4ba
Hamza Susam <hamza.susam@jetlink.io>
2022-09-10 23:06:55 +0300

    sql connection and get for departments and employees

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +12,22 @@ namespace WebAPI.Controllers
 {
     public class DepartmentController : ApiController
     {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"SELECT DepartmentID, DepartmentName FROM dbo.Departments";
+
+            var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString);
+            var command = new SqlCommand(query, con);
+
+            using (var dataAdapter = new SqlDataAdapter(command))
+            {
+                command.CommandType = CommandType.Text;
+                dataAdapter.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
     }
 }
--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
 
 namespace WebAPI.Controllers
 {
-    public class EmployeeController : Controller
+    public class EmployeeController : ApiController
     {
-        // GET: Employee
-        public ActionResult Index()
+        public HttpResponseMessage Get()
         {
-            return View();
+            DataTable table = new DataTable();
+
+            string query = @"SELECT EmployeeID, EmployeeName, Department, MailID, CONVERT(varchar(10), DOJ, 120) as DOJ FROM dbo.Employees";
+
+            var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString);
+            var command = new SqlCommand(query, con);
+
+            using (var dataAdapter = new SqlDataAdapter(command))
+            {
+                command.CommandType = CommandType.Text;
+                dataAdapter.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
         }
     }
 }
